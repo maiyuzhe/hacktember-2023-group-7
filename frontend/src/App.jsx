2811bed9c5ae24b6215f4a2a136f3b6f8b7a5deb
michael pm <michaelpmaurerii@gmail.com>
2023-10-01 20:48:06 -0400

    some cleanup and organization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
-import Home from './Home';
-import About from './About';
-import Courses from './Courses';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Courses from './Pages/Courses';
+import NavBar from './NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="about" element={ <About/> } />
