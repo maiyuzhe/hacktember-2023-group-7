0d7542c9f41d02a318af94f6bc323bee041d64b3
Michael Stafford <michaelstafford808@gmail.com>
2023-10-03 21:55:25 -0400

    attempted to add figma styling code to multiple components

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ImageQueue from '../Components/Home/ImageQueue'
-
+import React from "react";
+// import { Button } from "./Button";
+import Frame from "../Components/Home/Togglex";
 
 const Home = () => {
-  return (
-    <div class="home-page">
-      Home
-      <div class="carousel-home">
-        <ImageQueue/>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="hero-section">
+            <div className="overlap">
+                {/* <Button className="button-instance" mode="default" type="default" /> */}
+                <p className="AI-powered-learning">Ai-powered Learning: Bridging Knowledge And Possibilities</p>
+            </div>
+            <div className="frame-wrapper">
+                <Frame
+                    className="frame-1"
+                    darkLightClassName="frame-instance"
+                    ellipseClassName="frame-1-instance"
+                    property1="default"
+                />
+            </div>
+        </div>
+    );
+};
 
 export default Home
